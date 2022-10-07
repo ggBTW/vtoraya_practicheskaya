c0d231e97be8ad585e8c992d905386d3c0b49418
Gg_btw <questutrom@gmail.com>
2022-10-07 17:33:29 +0300

    C:/Program Files/Git/

---
+++ 2-я практическая/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _2_я_практическая
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
